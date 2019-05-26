ac26fc61834c53aaf344f2152cd9de94feaf53a2
Marcin Ptok <marcin111100@gmail.com>
2019-05-27 00:05:33 +0200

    Add edit and delete event

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ closeAddForm.addEventListener('click', () => {
 addFormBtn.addEventListener('click', () => {
   document.getElementById('errorContainer').innerHTML = "";
   let inputs = document.querySelectorAll(".addFormEl:not(.input)");
-  console.log(inputs);
+  // console.log(inputs);
 
   try {
     for (let i = 0; i < inputs.length; i++) {
@@ -123,16 +123,18 @@ addFormBtn.addEventListener('click', () => {
   }
 });
 
-function addEventDOM(event) {
+function addEventDOM(event, index) {
   let container = document.createElement("div");
+  let infoContainer = document.createElement("div");
   container.className = "eventContainer border";
+  infoContainer.className = "eventInfoContainer";
 
   let divImg = document.createElement("div");
   divImg.className = "eventImg"
   img = document.createElement("img");
   img.setAttribute('src', 'img/' + event.type + ".png");
   divImg.appendChild(img);
-  container.appendChild(divImg);
+  infoContainer.appendChild(divImg);
 
   let divInfo = document.createElement("div");
   divInfo.className = "eventInfo";
@@ -145,12 +147,21 @@ function addEventDOM(event) {
   info.className = "eventName";
   info.innerHTML = event.name;
   divInfo.appendChild(info);
-  container.appendChild(divInfo);
+  infoContainer.appendChild(divInfo);
 
   let edit = document.createElement("div");
   edit.classList = "eventBtn";
   edit.innerHTML = "<ion-icon name='create'></ion-icon> edit";
-  container.appendChild(edit);
+  edit.setAttribute("onClick", "editEvent(" + index + ")");
+  infoContainer.appendChild(edit);
+  container.appendChild(infoContainer);
+
+  // let evDesc = document.createElement("div");
+  // evDesc.className = "eventDescription";
+  // evDesc.innerHTML = event.desc;
+  // container.appendChild(evDesc);
+
+
 
   document.getElementById("eventsList").appendChild(container);
 }
@@ -161,14 +172,16 @@ function refreshUpcomingEvent(event) {
     cont.removeChild(cont.firstChild);
   }
   let container = document.createElement("div");
-  container.className = "eventContainer";
+  let infoContainer = document.createElement("div");
+  container.className = "eventContainer border";
+  infoContainer.className = "eventInfoContainer";
 
   let divImg = document.createElement("div");
   divImg.className = "eventImg"
   img = document.createElement("img");
   img.setAttribute('src', 'img/' + event.type + ".png");
   divImg.appendChild(img);
-  container.appendChild(divImg);
+  infoContainer.appendChild(divImg);
 
   let divInfo = document.createElement("div");
   divInfo.className = "eventInfo";
@@ -181,24 +194,123 @@ function refreshUpcomingEvent(event) {
   info.className = "eventName";
   info.innerHTML = event.name;
   divInfo.appendChild(info);
-  container.appendChild(divInfo);
+  infoContainer.appendChild(divInfo);
 
   let edit = document.createElement("div");
   edit.classList = "eventBtn";
   edit.innerHTML = "<ion-icon name='create'></ion-icon> edit";
-  container.appendChild(edit);
+  edit.setAttribute("onClick", "editEvent(0)");
+  infoContainer.appendChild(edit);
+  container.appendChild(infoContainer);
+
+  // let evDesc = document.createElement("div");
+  // evDesc.className = "eventDescription";
+  // evDesc.innerHTML = event.desc;
+  // container.appendChild(evDesc);
+
 
   document.getElementById("upcomingEvent").appendChild(container);
 }
 
+// document.getElementsByClassName("deleteElement")[0].addEventListener('click', function removeEl() {
+//   let deleteEv = confirm("Confirm delete event");
+//   console.log(deleteEv)
+//   if (deleteEv == true) {
+//     if (index == 0) events.shift();
+//     if (index > 1) events.splice(index, 1);
+//   }
+//   events.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
+//   localStorage.setItem("eventsList", JSON.stringify(events));
+//   document.getElementsByClassName("deleteElement")[0].removeEventListener('click', removeEl());
+//   if (events.length > 0) refreshEventsList();
+// })
+
+function editEvent(index) {
+  let editForm = document.getElementById("editForm");
+  editForm.style.display = "block";
+  editForm.style.animation = "showEl 0.3s cubic-bezier(0.21, 1.09, 1, 1)";
+
+  getDataFromEvent(events[index]);
+
+  document.getElementsByClassName("deleteElement")[0].addEventListener('click', function _removeEl() {
+    if (index == 0) events.shift();
+    if (index > 1) events.splice(index, 1);
+    events.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
+    refreshEventsList();
+    if (events.length == 0) {
+      localStorage.setItem("eventsList", null);
+      location.reload();
+    } else localStorage.setItem("eventsList", JSON.stringify(events));
+    document.getElementsByClassName("deleteElement")[0].removeEventListener('click', _removeEl)
+  })
+
+
+  document.getElementById("editFormBtn").addEventListener('click', () => {
+    document.getElementById('errorEditContainer').innerHTML = "";
+    let inputs = document.querySelectorAll(".editFormEl:not(.input)");
+    // console.log(inputs);
+
+    try {
+      for (let i = 0; i < inputs.length; i++) {
+        const input = inputs[i];
+        if (!input.value || input.value == "") {
+          throw "All fields must be filled!";
+        }
+      }
+
+      events[index].type = inputs[0].value
+      events[index].date = inputs[1].value
+      events[index].name = inputs[2].value
+      events[index].place = inputs[3].value
+      events[index].desc = inputs[4].value
+
+      events.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
+      localStorage.setItem("eventsList", JSON.stringify(events));
+      refreshEventsList();
+
+      document.getElementById("saved").style.animation = "notification 4s cubic-bezier(0.21, 1.09, 1, 1)";
+      setTimeout(() => {
+        document.getElementById("editForm").style.animation = "hideEl 0.5s cubic-bezier(0.21, 1.09, 1, 1)";
+        document.getElementById("saved").style.animation = "";
+        setTimeout(() => {
+          document.getElementById("editForm").style.display = "none";
+        }, 500);
+      }, 4000)
+    } catch (error) {
+      document.getElementById('errorEditContainer').innerHTML = error;
+    }
+  });
+
+  document.getElementsByClassName("closeAddForm")[1].addEventListener('click', () => {
+    editForm.style.animation = "hideEl 0.5s cubic-bezier(0.21, 1.09, 1, 1)";
+    setTimeout(() => {
+      editForm.style.display = "none";
+    }, 500);
+  });
+}
+
+function getDataFromEvent(event) {
+  let inputs = document.querySelectorAll(".editFormEl");
+  // console.log(inputs);
+  inputs[0].value = event.type;
+  document.getElementsByClassName("typeAddImg")[1].setAttribute('src', 'img/' + event.type + ".png");
+  inputs[1].value = event.date;
+  inputs[2].value = event.date;
+  inputs[3].value = event.name;
+  inputs[4].value = event.place;
+  inputs[5].value = event.desc;
+
+}
+
+
 function loadEventFromLS() {
   events = JSON.parse(localStorage.getItem("eventsList"));
 
-  if (events == null) {
+  if (events == null || events.lenght == 0) {
     noEvents(0);
     noUpcomingEvent();
-    events = []
-    return;
+    events = [];
+    return 0;
   } else {
     events.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
     if (events.length == 1) {
@@ -207,23 +319,30 @@ function loadEventFromLS() {
     refreshUpcomingEvent(events[0]);
     for (i = 1; i < events.length; i++) {
       //posortowane wyżej 1 do upcomming a reszta niżej jeżeli nie ma to wiadomość tu i tu
-      addEventDOM(events[i]);
+      addEventDOM(events[i], i);
     }
   }
 }
 
 function refreshEventsList() {
   let container = document.getElementById("eventsList");
+  console.log(container)
+  console.log(events)
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
-  if (events.length == 1) {
+
+  if (events.length == 0 || events == null) {
+    noEvents();
+    noUpcomingEvent();
+    return;
+  } else if (events.length == 1) {
     noEvents(1);
   }
   refreshUpcomingEvent(events[0]);
   if (events.length > 1) {
     for (i = 1; i < events.length; i++) {
-      addEventDOM(events[i]);
+      addEventDOM(events[i], i);
     }
   }
 }
